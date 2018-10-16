61a54b2ddc25e47e22545fb22271e119d8a0d83f
Bete Yemane <beteyemane@Betes-MacBook-Air.local>
2018-10-17 00:11:35 +0100

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,16 @@
 console.log("Up and running!");	
-
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
-
 console.log("User flipped " + cardOne);	
-console.log("User flipped " + cardTwo);							
+console.log("User flipped " + cardTwo);	
+
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+
+var cardOne; = [0];
+cardsInPlay.push("cardOne");
+console.log("User flipped queen");
+
+var cardTwo = [2];
+cardsInPlay.push("cardTwo");
+console.log("User flipped king");
+
+if (cards.length === "2");
