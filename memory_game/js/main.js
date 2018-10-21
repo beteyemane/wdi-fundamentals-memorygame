2065193a8bed426a6ee97224f7ad5892f740ed0d
Bete Yemane <beteyemane@Betes-MacBook-Air.local>
2018-10-21 18:18:22 +0100

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,16 +1,22 @@
-console.log("Up and running!");	
-console.log("User flipped " + cardOne);	
-console.log("User flipped " + cardTwo);	
 
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
+var cardOne = cards[0];
+var cardTwo = cards[2];
 
-var cardOne; = [0];
-cardsInPlay.push("cardOne");
-console.log("User flipped queen");
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
 
-var cardTwo = [2];
-cardsInPlay.push("cardTwo");
-console.log("User flipped king");
+console.log("User flipped " + cardOne);
+console.log("Use flipped " + cardTwo);
 
-if (cards.length === "2");
+if (cardsInPlay.length === 2) {
+
+}	else if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+}	else (cardsInPlay[0] !== cardsInPlay[2])  
+		alert("Sorry, try again.");
+
+
+
+
