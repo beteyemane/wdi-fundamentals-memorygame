ff3174baa3e79677a4ff5ae3b2d8ec1f60629438
Bete Yemane <beteyemane@Betes-MacBook-Air.local>
2018-11-13 12:11:23 +0000

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,26 +1,27 @@
 
 var cards = [
-{
-	rank: "queen",
-	suit: "hearts",
-	cardImage: "images/queen-of-hearts.png"
-},
-{
-	rank: "queen",
-	suit: "diamonds",
-	cardImage: "images/queen-of-diamonds.png"	
-},
-{
-	rank: "king",
-	suit: "hearts",
-	cardImage: "images/king-of-hearts.png"
-},
-{
-	rank: "king",
-	suit: "diamonds",
-	cardImage: "images/king-of-diamonds.png"
-},
+	{
+		rank: "queen",
+		suit: "hearts",
+		cardImage: "images/queen-of-hearts.png"
+	},
+	{
+		rank: "queen",
+		suit: "diamonds",
+		cardImage: "images/queen-of-diamonds.png"	
+	},
+	{
+		rank: "king",
+		suit: "hearts",
+		cardImage: "images/king-of-hearts.png"
+	},
+	{
+		rank: "king",
+		suit: "diamonds",
+		cardImage: "images/king-of-diamonds.png"
+	},
 ];
+
 var cardsInPlay = [];
 
 var checkForMatch = function() {
@@ -29,20 +30,31 @@ var checkForMatch = function() {
 	} else { 
 		alert("Sorry, try again.");
 	}
-};
+}
 
-var flipCard = function(cardId) {
+var flipCard = function() {
+	var cardId = this.getAttribute("data-id");
 	cardsInPlay.push(cards[cardId].rank);
-	if (cardsInPlay.length === 2) {	
-		checkForMatch();
-	}
+		this.setAttribute('src', cards[cardId].cardImage);
+		if (cardsInPlay.length === 2) {	
+	checkForMatch();
+}
 	console.log("User flipped " + cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
-};
+}
 
-flipCard(0);
-flipCard(2);
+var createBoard = function() {
+	for (var i = 0; i < cards.length; i++) {
+	var cardElement = document.createElement('img');
+	cardElement.setAttribute('src', 'images/back.png');
+	cardElement.setAttribute('data-id', i);
+	cardElement.addEventListener('click', flipCard);
+	document.getElementById("game-board").appendChild(cardElement);
+	}
+}
+
+createBoard();
 
 
 
